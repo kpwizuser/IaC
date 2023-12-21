62176553291df6ffbe0b3684fe4626deba68a618
Peter-Gess <gesspeter@gmail.com>
2023-12-21 13:30:17 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIAJSIE37KKMHXI3BJQ"
+  secret_key = "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"
+}
+
 resource "aws_s3_bucket" "wiz" {
   bucket = "my-tf-test-bucket"
 
