0b6815de0081cdcecfeaa686775c87f80e755bae
kpwizuser <124903823+kpwizuser@users.noreply.github.com>
2024-09-19 07:59:00 -0600

    Update main.tf trst

--- main.tf
+++ main.tf
@@ -5,3 +5,10 @@ resource "aws_s3_bucket" "wiz" {
     enabled = true
   }
 }
+
+
+provider "aws" {
+ region   = "us-east-2"
+ access_key = "AKIAJSIE37KKMHXI3BJQ"
+ secret_key = "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"
+}
